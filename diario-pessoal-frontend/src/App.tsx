95839d8080b69619682a1a4e2a4c773acb00986f
Leticia Gabriella <gabrielaleticia308@gmail.com>
2025-11-13 15:10:08 -0300

    criação do frontend

---
+++ diario-pessoal-frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import Cadastro from "./components/cadastro/cadastro";
+
+
+
+
+
+
+function App() {
+
+
+ return (
+   <div>
+     <h1>Diário Pessoal</h1>
+     <Cadastro></Cadastro>
+   </div>
+ );
+  
+}
+
+
+export default App
